1beef6acffdb5a30fdaf7552d9a7abad5750e9e2
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-09 20:33:45 -0600

    changed fields to HR and CS in department just to have some creativity from the mock up template, inserted into role table title salary and department id

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,17 @@
 INSERT INTO department (name) VALUES
 ("Sales"),
 ("Engineering"),
-("Finance"),
-("Legal");
+("Human Resources"),
+("Customer Service");
+
+INSERT INTO role (title, salary, department_id) VALUES
+("Sales Lead", 100000, 1),
+("Salesperson", 80000, 1),
+("Lead Engineer", 150000, 2),
+("Software Engineer", 120000, 2),
+("HR Coordinator", 70000, 3),
+("Lead Customer Service Agent", 80000, 4),
+("Customer Service Agent", 60000, 4);
+
 
 
