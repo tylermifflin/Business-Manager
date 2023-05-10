d39be890ca504569ce27ef5de32c2d0b01c7965d
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-09 19:40:49 -0600

    created a table called role, used ud, title, salary, used department_id as foreign key to reference the id in department table

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
