4920734be77b9ad7228ddb5c9cb8953ab53020b5
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-09 21:39:49 -0600

    trying to figure out if i even need express or how to link inquirer and mysql

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+// calling express, inquirer, fs, path, and mysql
 const express = require('express');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 const mysql = require('mysql2');
+
+// connect to database
+const db = mysql.createConnection(
+    {   
