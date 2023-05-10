9fc8fb9f5eec4514add23a68d4a3413f8ab0a4b7
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-09 19:48:36 -0600

    created table for employee with id first name last name role id manager id foreign key role id to connect to id in role table, foreign key manager id to reference id in employee table

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,14 @@ CREATE TABLE role (
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
