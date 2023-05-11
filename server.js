ebac878177e0e6cc9d86566144fbb6e94b017aea
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-10 18:05:51 -0600

    set up the connection to mysql manager_db

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 // calling express, inquirer, fs, path, and mysql
-const express = require('express');
+//const express = require('express');
 const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
+//const fs = require('fs');
+//const path = require('path');
 const mysql = require('mysql2');
 
 // connect to database
 const db = mysql.createConnection(
-    {   
+    { host: 'localhost',
+     // MySQL username,
+     user: 'root',
+    // MySQL password
+    password: '',
+    database: 'manager_db'
+    },
+    console.log('Connected to the manager_db database.')
+);
