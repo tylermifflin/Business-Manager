b5971f8a2079fffdc5d895dfc02fc01e18792df3
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-10 19:47:19 -0600

    was getting errors when trying to run mysql schema so fixed them, got rid of DELETE SET NULL and NOT NULL on all three id values for each table

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,26 @@ CREATE DATABASE manager_db;
 USE manager_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-    ON DELETE SET NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
-    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
-    ON DELETE SET NULL
 );
 
 
