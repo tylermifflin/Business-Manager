9b1be75df735c8ed9b5dc538b0936fb8cec37bb5
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-10 18:17:10 -0600

    set up the .then to open up whatever option the user chooses and grab the database information

--- server.js
+++ server.js
@@ -26,3 +26,33 @@ function start() {
         message: 'What would you like to do?',
         choices: ['View All Employees', 'View All Departments', 'View All Roles', 'Add Employee', 'Add Department', 'Add Role', 'Update Employee Role', 'Exit']
     })
+    .then((answer) => {
+        switch (answer.start) {
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                db.end();
+                break;
+        }
+    }
+    )
+}
