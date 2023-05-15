310c6330d771794b74b0d727ef2f354145a9cb28
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-15 17:47:10 -0600

    got my employee table to look how it is supposed to

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,9 @@
 SELECT role.id, role.title, department.name AS department, role.salary
 FROM department
-JOIN role ON department_id = department.id;
+JOIN role ON department_id = department.id; 
+
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager_id AS manager
+FROM employee
+JOIN role ON role_id = role.id
+JOIN department ON department_id = department.id;
