40ef18395a63908eb9178d2341667a950172a146
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-15 22:36:08 -0600

    inserted the query in server.js to viewAllEmployees and see the desired fields

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ function viewAllRoles() {
 
 // function to view all employees and view the table in the console, using my 2nd query.sql to join the tables for employee to look as desired
 function viewAllEmployees() {
-    db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager_id AS manger FROM employee JOIN role ON role_id = role.id JOIN department ON department_id = department.id', function (err, results) {
+    db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, manager.last_name) AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id', function (err, results) {
         console.table(results);
         start();
     });
