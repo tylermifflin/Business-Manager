ac0f52fd045c197768ec20227aee18271b1d6e04
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-15 22:25:35 -0600

    got the query to work to show the manager name instead of manager id when view all employees

--- db/query.sql
+++ db/query.sql
@@ -3,7 +3,8 @@ FROM department
 JOIN role ON department_id = department.id; 
 
 
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager_id AS manager
-FROM employee
-JOIN role ON role_id = role.id
-JOIN department ON department_id = department.id;
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+FROM employee 
+JOIN role ON employee.role_id = role.id
+JOIN department ON role.department_id = department.id
+LEFT JOIN employee manager ON employee.manager_id = manager.id;
